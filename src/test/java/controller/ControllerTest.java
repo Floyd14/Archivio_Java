bb64431965dc92d051b9ff82f8336bf7bdc63f42
Floyd <andreafloyd14@icloud.com>
2022-11-22 18:50:10 +0100

    test try

--- src/test/java/controller/ControllerTest.java
+++ src/test/java/controller/ControllerTest.java
@@ -1,5 +1,17 @@
-package controller;
+package java.controller;
+
+import controller.Controller;
+import interfaces.StorageType;
+import model.Movie;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class ControllerTest {
 
+    @Test
+    public void getMoviesShouldReturnListOfMovies() {
+
+
+    }
 }
---
+++ src/test/view/MenuTest.java
@@ -0,0 +1,13 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    @Test
+    void showMenu() {
+
+    }
+}
