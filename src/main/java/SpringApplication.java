b6bf31d4330b33a779a713d9fbb52a41886923e3
Floyd <andreafloyd14@icloud.com>
2022-12-07 18:50:49 +0100

    changed database and fixed queries in MySQLStorage

---
+++ src/main/java/SpringApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class, args);
+    }
+
+}
